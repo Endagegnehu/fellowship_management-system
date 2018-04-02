8d272f202cd4dcb68024f94fc435418cd31445d4
adonias <adonias@gmail.com>
2018-04-02 11:10:28 -1000

    Revert "table population not working"
    This reverts commit 4e762b0348a1d102d3a62e6aa87982037c3e83cd.

--- src/fello/controller/MemberManager.java
+++ src/fello/controller/MemberManager.java
@@ -8,6 +8,31 @@ public class MemberManager{
    Member member = new Member();
    Model model=Model.getInstance();
    Team team = new Team();
+   private static final int FRIST_NAME_COL=1;
+   private static final int LAST_NAME_COL =2;
+   private static final int BLOCK_NUM_COL=3;
+   private static final int DORM_NUM_COL=4;
+   private static final int SCHOOL_COL=5;
+   private static final int PROGRAM_COL=6;
+   private static final int GENDER_COL=7;
+   private static final int YEAR_COL=8;
+   private static final int REGION_COL=12;
+   private static final int TOWN_COL=13;
+   private static final int CITY_COL=14;
+   private static final int PHONE_COL=15;
+   private static final int EMAIL_COL=16;
+   private static final int GNAME_COL=17;
+   private static final int GPHONE_COL=18;
+   private static final int GTYPE_COL=19;
+   private static final int CNAME_COL=20;
+   private static final int CPNAME_COL=21;
+   private static final int CPPHONE_COL=22;
+   private static final int  DEPARTMENT_COL=23;
+   
+   private String[] columnNames = {"Fristname","Lastname","BlockNumber","DormNumber"
+   ,"School","Program","Gender","Year","Region","Town","City","phone","EMail","GuardianName",
+   "GuardianPhone","GuardianType","ChurchName","ChurchPasterName","ChurchPasterPhone","Department"};
+   
    String columns = "`frist_name`," +"`last_name`," +"`block_number`," +"`dorm_number`," +"`school`," +
                     "`program`,"+"`gender`," + "`year`,"+"`region`," +"`town`," +"`city`," +"`phone`," +"`e_mail`," +"`guardian_name`,"
                     +"`guardian_phone`," + "`guardian_type`," +"`church_name`," +"`church_pastor_name`," +"`church_pastor_phone`," +"`Department`";
